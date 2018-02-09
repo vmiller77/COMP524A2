411be64e0b8042976ac232bed27ee73e27c6b5f7
vmiller77 <vnm@live.unc.edu>
2018-02-09 00:18:24 -0500

    Made changes

--- tree.c
+++ tree.c
@@ -1,41 +1,64 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+
+void makeRoot(char* name, char *family[]);
 
 int main() {
-  //allocate space for the root
+  //make array to hold strings for family so has 7 spots
+  char family[7][13]; //strs[i] = malloc(strlen("foo") + 1);
 
+  char name[12];
   printf("What is your name? ");
-  fgets(name,12,stdin);
-  //store name into root
+  scanf("%s",&name);
+  printf("%s\n",name);
 
+  //store name into root
+  makeRoot(name,family);
 
   //start running program until quits
   while(1){
-    fgets(input,33,stdin);
+    char input[33];
+    scanf("%s", &input);
+    printf("%s",input);
 
     if(strcmp(input,"quit")==0){//if quit then quit program
-      return;
-    }else{//if father then addFather
-
-    }else{//if mother then addMother
-
-    }else{//if print then print tree
-
-    }else{//if delete then delete node
-
+      exit(0);
+    }else if(1){//if father then addFather
+      printf("father");
+    }else if(1){//if mother then addMother
+      printf("mother");
+    }else if(1){//if print then print tree
+      printf("print");
+    }else if(1){//if delete then delete node
+      printf("delete");
     }else{//if neither return an error message
       printf("Sorry, you put an invalid command!");
     }
   }
-  
+
 }
 
 //methods
-void makeRoot(){
+void makeRoot(char* name, char *family[]){
+  family[0]=name;
+}
+//addNode father=1 if father, 0 if mother
+void addNode(char* name, int father){
+  //check if it can be placed there
+  //if yes then allocate memory and place it there and add pointer
+
+  //if not return an error message
 
 }
-//addNode
-
 //printTree
+void printTree(){
 
+}
 //deleteNode
+void deleteNode(char* name){
+  //check to see if it can be deleted
+  //if yes the delete it and free memory and remove pointer
+
+  //if not then error message
+}
