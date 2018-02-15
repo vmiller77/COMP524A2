6271246cda7e17de7b5f270b9f7978ca1db64aa1
vmiller77 <vnm@live.unc.edu>
2018-02-14 23:58:13 -0500

    GOT THE ADDNODE TO WORK!

---
+++ treemake.h
@@ -0,0 +1,6 @@
+void makeUser(char* name, char *family[]);
+void printTree();
+void addNode(char* child, char* parent, int father, char *family[]);
+void deleteNode(char* name, char *family[]);
+int exists(char* name, char *family[]);
+void printAll(char *family[]);
